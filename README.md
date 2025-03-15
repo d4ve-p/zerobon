52d3f8733c55578cd10a14805c6d19fbbd2d7bcd
Dave P <davep1405@gmail.com>
2025-03-15 20:19:35 +0700

    feature show items

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # zerobon
+# show items
--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,6 @@ Route::middleware(['auth'])->group(function () {
     Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
 });
 
+
+Route::get('/homepage')
 require __DIR__.'/auth.php';
