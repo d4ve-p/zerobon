2ac95ef6dadb189024b1996a572f871401da2a45
Dave P <davep1405@gmail.com>
2025-03-30 17:04:40 +0700

    disables default

--- routes/auth.php
+++ routes/auth.php
@@ -4,20 +4,19 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
-Route::middleware('guest')->group(function () {
-    Volt::route('login', 'auth.login')
-        ->name('login');
+// Route::middleware('guest')->group(function () {
+//     Volt::route('login', 'auth.login')
+//         ->name('login');
 
-    Volt::route('register', 'auth.register')
-        ->name('register');
+//     Volt::route('register', 'auth.register')
+//         ->name('register');
 
-    Volt::route('forgot-password', 'auth.forgot-password')
-        ->name('password.request');
+//     Volt::route('forgot-password', 'auth.forgot-password')
+//         ->name('password.request');
 
-    Volt::route('reset-password/{token}', 'auth.reset-password')
-        ->name('password.reset');
-
-});
+//     Volt::route('reset-password/{token}', 'auth.reset-password')
+//         ->name('password.reset');
+// });
 
 Route::middleware('auth')->group(function () {
     Volt::route('verify-email', 'auth.verify-email')
