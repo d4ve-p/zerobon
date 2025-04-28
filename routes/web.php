7085bc503248ce02532e1e2c07d6f621a0e53250
Dave P <65463983+d4ve-p@users.noreply.github.com>
2025-04-28 15:49:59 +0700

    added routes

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,82 @@ Route::get('products/edit/{id}', function ($id) {
 Route::get('products/delete/{id}', [ProductController::class, 'delete'])
     ->name('delete-product');
 
+// TODO: Change empty function() into the appropriate function/controller
+
+// Articles
+Route::prefix('articles')->group(function() {
+    Route::get('/', function() { })
+        ->name('articles');
+    Route::get('/{id}', function() {  })
+        ->name('articles-detail'); 
+});
+
+// Social Activities
+Route::prefix('social-activities')->group(function() {
+    Route::get('/', function() { })
+        ->name('social-activities');
+});
+
+Route::middleware(['auth'])->group(function() {
+    // Carts
+    Route::prefix('carts')->group(function() {
+        Route::view('/', function() { })
+        ->name('carts');
+    });
+
+
+    // Purchases
+    Route::prefix('purchases')->group(function() {
+        Route::view('/', function() { })
+            ->name('purchases');
+        Route::get('/{id}', function() {})
+            ->name('purchase-detail');
+    });
+
+    // Forums
+    Route::prefix('forum')->group(function() {
+        Route::get('/', function() { })
+            ->name('forum'); // Controller will handle searches and normal forum here
+        Route::view('/create', function() { })
+            ->name('forum-create');
+        Route::post('create', function() {})
+            ->name('forum-create-post');
+        Route::post('/reply', function() {})
+            ->name('forum-reply-post');
+    });
+
+    // Vouchers
+    Route::prefix('vouchers')->group(function() {
+        Route::get('/', function() { })
+            ->name('vouchers');
+        Route::post('/use', function() { })
+            ->name('vouchers-use-post');
+    });
+
+    // Carbon Footprint
+    Route::prefix('carbon-foorprints')->group(function() {
+        Route::get('/', function() {})
+            ->name('carbon-footprint');
+    });
+
+    // Donation
+    Route::prefix('donation')->group(function() {
+        Route::get('/', function() {})
+            ->name('donate');
+        Route::post('/', function() {})
+            ->name('donate-post');
+    });
+
+    // Reward
+    Route::prefix('reward')->group(function() {
+        Route::get('/', function() { })
+            ->name('reward');
+        Route::post('/use', function() { })
+            ->name('reward-user-post');
+    });
+});
+
+
 
 Route::middleware(['auth'])->group(function () {
     Route::redirect('settings', 'settings/profile');
