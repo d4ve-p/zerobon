dbbc7db704d31adb0dfaecd830b9e6dcac7b0a8a
Dave P <65463983+d4ve-p@users.noreply.github.com>
2025-05-21 11:35:26 +0700

    View: to-do challenge

---
+++ resources/views/challenge/challenge.blade.php
@@ -0,0 +1,37 @@
+@extends('components.layouts.app.home-layout')
+
+@section('content')
+<div class="flex flex-1 bg-[var(--color-cream-500)] justify-center w-full">
+    <div class="w-[1111px] flex-col items-center box-border">
+        <p class="py-[50px] text-[50px] font-semibold text-[var(--color-green-900)] text-center">To-Do-Challenge</p>
+        <div class="flex w-full justify-end mb-[50px]">
+            <button class="text-[24px] font-semibold bg-[var(--color-green-700)] text-white px-6 py-4 rounded-[15px]">Check Challenge Approval</button>
+        </div>
+        {{-- TODO: Get all unattempted challenges --}}
+        <div class="flex flex-col gap-7">
+            <div class="flex flex-col">
+                <div class="bg-white border-[var(--color-green-500)] rounded-[15px] drop-shadow-lg border-2 flex w-full justify-between h-[96px] items-center text-[var(--color-green-700)] px-8 box-border">
+                    <p class="text-[25px] font-semibold ">
+                        Go to TreeFund and Donate Tree
+                    </p>
+                    <div class="flex items-center gap-2">
+                        <p class="font-bold text-[20px]">[+ 15 points ]</p>
+                        <a href="{{ url("/challenge/test") }}" class="font-semibold text-[25px] underline">Details</a>
+                    </div>
+                </div>
+            </div>
+            <div class="flex flex-col">
+                <div class="bg-white border-[var(--color-green-500)] rounded-[15px] drop-shadow-lg border-2 flex w-full justify-between h-[96px] items-center text-[var(--color-green-700)] px-8 box-border">
+                    <p class="text-[25px] font-semibold ">
+                        Go to TreeFund and Donate Tree
+                    </p>
+                    <div class="flex items-center gap-2">
+                        <p class="font-bold text-[20px]">[+ 15 points ]</p>
+                        <a href="{{ url("/challenge/test") }}" class="font-semibold text-[25px] underline">Details</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
