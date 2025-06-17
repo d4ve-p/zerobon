9418fd8437cd0e82fd986919527b6320b3fb62db
Sherlyjia29 <sherlyjia29@gmail.com>
2025-06-17 17:11:11 +0700

    fix: banner ecomarket

--- resources/views/products/products.blade.php
+++ resources/views/products/products.blade.php
@@ -53,7 +53,7 @@
     <div class="w-full flex box-border p-10 pl-[100px] h-[592px]">
         <img id="popup-image" src="{{asset("eco-market.png")}}" width="900px"/>
         <div class="flex h-full flex-1 w-full relative">
-            <div class="flex items-center justify-center flex-col h-full absolute left-[-50%] top-[50%] translate-y-[-50%]">
+            <div class="flex items-center justify-center flex-col h-full w-full">
                 <p class="text-[60px] text-[var(--color-green-700)] font-bold mb-10">Let's Shop At EcoMarket!</p>
                 <a href="{{route("purchases")}}"><button class="w-[539px] h-[47px] text-white bg-[var(--color-green-700)] rounded-[30px] font-bold hover:cursor-pointer">Check Order Status</button></a>
             </div>
